464b2e83fa36e2241eac4d151630bc72fd896d64
Zac Smith <zrs3141592@gmail.com>
2019-08-11 17:07:56 -0600

    fix server from error

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const express = require("express");
+
+// other files
+const db = require("./data/db.js");
+
+// // global objects
+const server = express();
+const port = 4000;
+
+// // middleware
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).send(`
+    <h2>Lambda Web API II Challenge!</h2>
+  `);
+});
+
+// request handlers
+
+// GET
+
+// POST
+
+// DELETE
+
+// PUT
+
+// Listen
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
