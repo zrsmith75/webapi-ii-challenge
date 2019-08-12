4f4f98c055b4df519ed3372fce55ead7f109d2d5
Zac Smith <zrs3141592@gmail.com>
2019-08-11 23:11:06 -0600

    create postRoutes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
-
+const postRoutes = require("./posts/postRoutes.js");
+const db = require("./data/db.js");
 const server = express();
 const port = 4000;
 
+server.use(express.json());
+// /api/ route
+server.use("/api", postRoutes);
+
 server.get("/", (req, res) => {
-  res.status(200).send("Hello from exprss");
+  res.status(200).send("Hello from express");
 });
 
 server.listen(port, () => {
