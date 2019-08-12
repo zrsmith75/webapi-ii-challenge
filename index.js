7f8256646a100d31257998e066e776376966497f
Zac Smith <zrs3141592@gmail.com>
2019-08-11 22:21:45 -0600

    remake server, nodemon kept crashing

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
 const express = require("express");
 
-// other files
-const db = require("./data/db.js");
-
-// // global objects
 const server = express();
 const port = 4000;
 
-// // middleware
-server.use(express.json());
-
 server.get("/", (req, res) => {
-  res.status(200).send(`
-    <h2>Lambda Web API II Challenge!</h2>
-  `);
+  res.status(200).send("Hello from exprss");
 });
 
-// request handlers
-
-// GET /api/posts
-
-// GET /api/posts/:id
-
-// GET /api/posts/:id/comments
-
-// POST /api/posts
-
-// POST /api/posts/:id/comments
-
-// DELETE /api/posts/:id
-
-// PUT /api/posts/:id
-
-// Listen
 server.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`);
+  console.log(`server is listening on port ${port}`);
 });
---
+++ server.js
@@ -0,0 +1,38 @@
+// const express = require("express");
+
+// // other files
+// const db = require("./data/db.js");
+
+// // // global objects
+// const server = express();
+// const port = 4000;
+
+// // // middleware
+// server.use(express.json());
+
+// server.get("/", (req, res) => {
+//   res.status(200).send(`
+//     <h2>Lambda Web API II Challenge!</h2>
+//   `);
+// });
+
+// // request handlers
+
+// // GET /api/posts
+
+// // GET /api/posts/:id
+
+// // GET /api/posts/:id/comments
+
+// // POST /api/posts
+
+// // POST /api/posts/:id/comments
+
+// // DELETE /api/posts/:id
+
+// // PUT /api/posts/:id
+
+// // Listen
+// server.listen(port, () => {
+//   console.log(`Server is listening on port ${port}...`);
+// });
